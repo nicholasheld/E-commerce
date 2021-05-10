3189db18b6aaa04fbf03dbe4bd3307207117d063
nicholas <nicholaspheld@gmail.com>
2021-05-10 18:42:00 -0500

    updated columns in product tag and product js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -16,17 +16,29 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Product',
-        key: 'id',
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
       },
     },
-    tag_id: {
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Tag',
+        model: 'category',
         key: 'id',
       },
     },
